53f906953137fbb10437afc16b7aeb0f6728d460
Alankrith G <100883843+harshan1996@users.noreply.github.com>
2023-03-17 20:10:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let loadPercentage = document.querySelector(".percentage");
+let image = document.querySelector(".image");
+
+let currentLoad = 0;
+
+function loadIncrement() {
+  currentLoad++;
+  loadPercentage;
+  loadPercentage.style.opacity = scaling(currentLoad, 0, 100, 1, 0);
+  loadPercentage.innerText = currentLoad + "%";
+  image.style.filter = `blur(${scaling(currentLoad, 0, 100, 30, 0)}px)`;
+
+  if (currentLoad >= 100) {
+    clearInterval(Interval);
+  }
+}
+
+const Interval = setInterval(loadIncrement, 50);
+
+const scaling = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
